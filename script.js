c4dd99984ce59233ed1751831de03fb95cd073b2
applekaige <applekaige@me.com>
2026-04-20 20:19:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -31,13 +31,10 @@ function escapeHtml(text) {
 }
 
 async function fetchJson(url, options = {}) {
-  console.log("Fetching:", url);
   const res = await fetch(url, options);
-
   if (!res.ok) {
     throw new Error(`HTTP ${res.status}`);
   }
-
   return res.json();
 }
 
